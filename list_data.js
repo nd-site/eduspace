a536de91e9686ebeca1f650ec67a79e091e353d2
ND Co., Ltd <nd.co.ltd.10@gmail.com>
2025-12-30 00:34:37 +0700

    Add files via upload

--- list_data.js
+++ list_data.js
@@ -1,19 +1,50 @@
-/**
- * FILE DANH SÁCH BÀI KIỂM TRA (list_data.js)
- * Chứa thông tin các bài tập để hiển thị ngoài trang chủ.
- */
-
 const quizList = [
-    {
-        title: "Mẫu",
-        url: "mau",
-        description: "Bài kiểm tra mẫu để bạn tham khảo cách tạo đề.",
-        tag: "Mẫu"
-    },
     {
         title: "Đề cương GDQP-AN 10 - Học kỳ I năm học 2025-2026",
         url: "/eduspace/gdqp/10/on-tap-hk1-2526-v1",
         description: "Tổng hợp các câu hỏi ôn tập học kỳ I môn Giáo dục Quốc phòng và An ninh lớp 10 năm học 2025-2026 'Bản 1'",
         tag: "GDQP-AN",
+    },
+    {
+        title: "Đề cương KTPL 10 - Học kỳ I năm học 2025-2026",
+        url: "/eduspace/ktpl/10/hk1/on-tap-1",
+        description: "Tổng hợp các câu hỏi ôn tập học kỳ I môn Kinh tế Pháp luật lớp 10 năm học 2025-2026 'Bản 1'",
+        tag: "KTPL",
+    },
+    {
+        title: "CHƯA CÓ - Đề cương Lịch Sử 10 - Học kỳ I năm học 2025-2026",
+        url: "/eduspace/lichsu/10/hk1/on-tap-1",
+        description: "HIỆN CHƯA CÓ - Tổng hợp các câu hỏi ôn tập học kỳ I môn Lịch Sử lớp 10 năm học 2025-2026 'Bản 1'",
+        tag: "Lịch Sử",
+    },
+    {
+        title: "Đề cương Hóa Học 10 - Học kỳ I năm học 2025-2026",
+        url: "/eduspace/hoahoc/10/hk1/tinh-luong-ion-du-1",
+        description: "Tổng hợp các câu hỏi 'Tính lượng ion dư 1' ôn tập học kỳ I môn Hóa Học lớp 10 năm học 2025-2026 'Bản 1'",
+        tag: "Hóa Học",
+    },
+    {
+        title: "Đề cương Hóa Học 10 - Học kỳ I năm học 2025-2026",
+        url: "/eduspace/hoahoc/10/hk1/tinh-luong-ion-du-2",
+        description: "Tổng hợp các câu hỏi 'Tính lượng ion dư 2' ôn tập học kỳ I môn Hóa Học lớp 10 năm học 2025-2026 'Bản 2'",
+        tag: "Hóa Học",
+    },
+    {
+        title: "Đề cương Hóa Học 10 - Học kỳ I năm học 2025-2026",
+        url: "/eduspace/hoahoc/10/hk1/tinh-luong-ion-du-3",
+        description: "Tổng hợp các câu hỏi 'Tính lượng ion dư 3' ôn tập học kỳ I môn Hóa Học lớp 10 năm học 2025-2026 'Bản 3'",
+        tag: "Hóa Học",
+    },
+    {
+        title: "Đề cương Hóa Học 10 - Học kỳ I năm học 2025-2026",
+        url: "/eduspace/hoahoc/10/hk1/tinh-luong-ion-du-4",
+        description: "Tổng hợp các câu hỏi 'Lượng ion dư 4' ôn tập học kỳ I môn Hóa Học lớp 10 năm học 2025-2026 'Bản 4'",
+        tag: "Hóa Học",
+    },
+    {
+        title: "Mẫu",
+        url: "/eduspace/mau",
+        description: "Bài kiểm tra mẫu để bạn tham khảo cách tạo đề.",
+        tag: "Mẫu"
     },
 ];
